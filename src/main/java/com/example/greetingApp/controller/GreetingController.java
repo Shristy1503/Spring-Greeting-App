81fbe4c24ff792d8d30895e6f4803b376101f012
Shristy1503 <shriimishra15@gmail.com>
2025-02-22 16:50:24 +0530

    [Shristy] Add.Servuce

--- src/main/java/com/example/greetingApp/controller/GreetingController.java
+++ src/main/java/com/example/greetingApp/controller/GreetingController.java
@@ -1,11 +1,14 @@
 package com.example.greetingApp.controller;
 
 import com.example.greetingApp.model.GreetingModel;
+import com.example.greetingApp.model.ServiceGreet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greet")
 public class GreetingController {
+   // private ServiceGreet greet;
     @GetMapping
     public GreetingModel getGreeting(){
         return new GreetingModel("Hello : this is get mapping");
@@ -24,4 +27,12 @@ public class GreetingController {
     public GreetingModel postGreeting(){
         return new GreetingModel("Hello : this is post mapping");
     }
+
+    //using service layer
+
+    private  ServiceGreet greetings;
+   public  GreetingController(ServiceGreet greetings){
+       this.greetings = greetings;
+   }
+
 }
